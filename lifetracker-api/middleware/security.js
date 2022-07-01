9f2fc8669c9d221ad2b3f0633b7dbf6ef2afc5a3
charlesxu27 <charlesxu27@gmail.com>
2022-07-02 01:25:21 +0200

    Update Readme + add walkthrough gif

--- lifetracker-api/middleware/security.js
+++ lifetracker-api/middleware/security.js
@@ -1,3 +1,8 @@
 const bcrypt = require("bcrypt")
 
-const pw = "supersecretpassword"
+const p = "supersecretpassword"
+
+bcrypt.hash(password, 6, (err, hashedPassword) => {
+    console.log(`Password is ${password}`)
+    console.log(`Hashed`)
+})
--- lifetracker-ui/src/components/LandingPage/LandingPage.jsx
+++ lifetracker-ui/src/components/LandingPage/LandingPage.jsx
@@ -6,9 +6,9 @@ export default function LandingPage() {
   return (
     <div className='landing-page'>
       <div className='left-div'>
-        <h1>Get your life on track</h1>
-        <p>Now with daily reminders, precise tracking of daily routines, detailed dashboards for your health. Lifetracker is here to help you become the best version of yourself.</p>
-        <Link className="sign-up-link" to='/register'>Sign Up Today</Link>
+        <h1>Become the Best Version of Yourself</h1>
+        <p>Now with daily reminders, precise tracking of daily routines, detailed dashboards for your health. Lifetracker is here to guide you day by day, step by step, bite by bite. We are totally not tracking your every move and selling your data to data brokers. Trust us. Because why wouldn't you?</p>
+        <Link className="sign-up-link" to='/register'>Sign Up Now</Link>
       </div>
       <div className='hero'>
         <img className='hero-img' src='https://images.unsplash.com/photo-1434494878577-86c23bcb06b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'/>
